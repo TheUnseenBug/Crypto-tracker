c0232547dc50ddfc365d5d179ee7c61424acc6c0
TheUnseenBug <dennis.granheimer@live.se>
2022-04-12 14:13:19 +0200

    comment

--- src/index.js
+++ src/index.js
@@ -26,26 +26,31 @@ function App() {
 	const filteredCoins = coins.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()));
 
 	return (
-		<div className="coin-app">
-			<div className="coin-search" />
-			<h1 className="coin-text">Search a currency</h1>
-			<form>
-				<input className="coin-input" type="text" onChange={handleChange} placeholder="Search" />
-			</form>
-
-			{filteredCoins.map((coin) => {
-				return (
-					<Coin
-						key={coin.id}
-						name={coin.name}
-						price={coin.current_price}
-						volume={coin.market_cap}
-						image={coin.image}
-						priceChange={coin.price_change_percentage_24h}
-						marketCap={coin.market_cap}
-					/>
-				);
-			})}
+		<div>
+			<div className="navBar">
+				<div className="text">
+					<h1 className="coin-text">Search a currency</h1>
+				</div>
+				<div className="coin-search" />
+				<form>
+					<input className="coin-input" type="text" onChange={handleChange} placeholder="Search" />
+				</form>
+			</div>
+			<div className="coin-app">
+				{filteredCoins.map((coin) => {
+					return (
+						<Coin
+							key={coin.id}
+							name={coin.name}
+							price={coin.current_price}
+							volume={coin.market_cap}
+							image={coin.image}
+							priceChange={coin.price_change_percentage_24h}
+							marketCap={coin.market_cap}
+						/>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
